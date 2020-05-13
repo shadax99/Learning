8c7db340a85ca07405b15d71f37ef24fe939d89f
Shadax99 <shadax99@hotmail.com>
2020-05-13 18:39:22 -0400

    Test

--- MyFirstTest.py
+++ MyFirstTest.py
@@ -1,6 +1,13 @@
+<<<<<<< Updated upstream
 import sys
 
 if __name__ == "__main__":
     print(f"Arguments count: {len(sys.argv)}")
     for i, arg in enumerate(sys.argv):
         print(f"Argument {i:>6}: {arg}")
+=======
+import os
+import netmiko
+
+
+>>>>>>> Stashed changes
